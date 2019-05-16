1f23d963c11be7ed5a3f4cb2a19d246624aab9ae
UstNana <41002698+UstNana@users.noreply.github.com>
2019-05-17 01:46:26 +0300

    Update work10.py

--- work10.py
+++ work10.py
@@ -1,30 +1,20 @@
-    
-# -*- coding: utf-8 -*-
-import json
+  import json
 
 d = {}
+descript = []
 new_list = list
 r = 0
-with open("newsafr.json", encoding="utf8") as json_file:
+with open("news.json", encoding="utf8") as json_file:
       data_json = json.load(json_file)
       tems = data_json["rss"]["channel"]["items"]
       for elements in tems:
-          for key, value in elements.items():
-              if key == "description":
+          descript = elements["description"]
 
-                for key in reversed(sorted(value.split())):
-                  if key:
-                    word = key
-                    r += 1
-                  if word not in d.keys():
-                    d[word] = []
-                  d[word].append(r)
-list_i = list
-for key, value in d.items():
-  sort_d = sorted([(key, len(key)) for (key,value) in d.items()])
-#print(w)
-shortest = ""
-for numbers in sort_d:
-  if len(numbers[0]) > len(shortest):
-    shortest = numbers[0]
-    print(shortest)
+          for word in descript.split():
+            if word not in d.keys():
+              d[word] = 1
+            else:
+              d[word] += 1
+
+print(sorted(d.items(), key=lambda x: (len(x[0]), x[1]), reverse=True ))
+print(sorted(d.items(), key=lambda x: x[1], reverse=True ))
